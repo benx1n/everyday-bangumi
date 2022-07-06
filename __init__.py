e5792c0657afa38b74e06c6dedd7fe907f89ee4a
benx1n <1119809439@qq.com>
2022-07-06 19:55:33 +0800

    fix NoneType

--- __init__.py
+++ __init__.py
@@ -13,7 +13,7 @@ from .html_render import html_to_pic
 from .browser import get_new_page
 from .utils import bytes2b64
 
-sv_help = '''发送开启 everyday_bangumi，自动将在每日零点推送当日番剧'''
+sv_help = '''发送开启 everyday_bangumi，自动将在每日晚八点推送当日番剧'''
 sv = Service('everyday-bangumi', manage_priv=priv.SUPERUSER, enable_on_default=True,help_ = sv_help)
 
 dir_path = Path(__file__).parent
@@ -27,14 +27,28 @@ headers = {
 }
 
 
-@sv.on_prefix("测试番剧推送")
-async def get_today_bangumi(bot, ev: CQEvent):
+@sv.on_prefix("查询番剧")
+async def send_today_bangumi(bot, ev: CQEvent):
+    text = str(ev.message).strip()
+    img = await get_today_bangumi(text)
+    await bot.send(ev,str(MessageSegment.image(bytes2b64(img))))
+
+@sv.scheduled_job('cron',hour='20')
+async def auto_send_daily_bangumi():
+#@sv.on_prefix("推送今日番剧")
+#async def auto_send_daily_bangumi(bot, ev: CQEvent):
+    img = await get_today_bangumi()
+    await sv.broadcast(str(MessageSegment.image(bytes2b64(img))), 'auto_send_daily_bangumi', 2)
+
+async def get_today_bangumi(text = '今天'):
     try:
+        if not text:
+            text = '今天'
         url = 'https://bgmlist.com/'
         data1,data2 = [],[]
         async with get_new_page() as page:
             await page.goto(url)
-            await page.click("text=今天")
+            await page.click(f"text={text}")
             soup = BeautifulSoup(await page.content(), 'html.parser')
             bangumi_list = soup.select('article')
             for each_bangumi in bangumi_list:
@@ -55,6 +69,7 @@ async def get_today_bangumi(bot, ev: CQEvent):
                 for each in play:
                     playWeb_list.append(each.string)
                 info['playWeb'] = playWeb_list
+                info['imgUrl'] = None
                 if info['bangumiId']:
                     async with httpx.AsyncClient(headers=headers) as client:
                         url = f"https://api.bgm.tv/v0/subjects/{info['bangumiId']}"
@@ -76,7 +91,7 @@ async def get_today_bangumi(bot, ev: CQEvent):
             template = env.get_template("main.html")
             content = await template.render_async(data)
             img =  await html_to_pic(content, wait=0, viewport={"width": 1200 ,"height": 100})
-            await bot.send(ev,str(MessageSegment.image(bytes2b64(img))))
+            return img
     except Exception:
         traceback.print_exc()
         return None
